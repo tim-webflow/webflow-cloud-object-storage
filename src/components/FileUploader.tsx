1db11eca18c19e86feb6870fc662876944e5935f
Tim McMackin <tim.mcmackin@webflow.com>
2026-04-03 08:24:24 -0400

    push to update to build in-site app

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -257,7 +257,7 @@ export default function FileUploader() {
           color: "#333",
         }}
       >
-        File Upload Demo 8:11
+        File Upload Demo 8:24
       </h2>
 
       {/* Upload Mode Toggle */}
